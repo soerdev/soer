fc8944e44eda2a04b599eace9f4740911b8f1a91
Evgeniy Sergeev <esergeev777@gmail.com>
2022-03-20 17:19:15 +0700

    feature: cancel from payment

--- apps/naris/src/app/pages/modules/payment/pay-form/pay-form.component.ts
+++ apps/naris/src/app/pages/modules/payment/pay-form/pay-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { MEDIUM_TIMEOUT_INTERVAL } from '../../../../../environments/constants';
 import { environment } from '../../../../../environments/environment';
@@ -10,19 +11,37 @@ import { AuthService } from '../../../../api/auth/auth.service';
   templateUrl: './pay-form.component.html',
   styleUrls: ['./pay-form.component.scss']
 })
-export class PayFormComponent {
+export class PayFormComponent implements OnChanges{
   public email = '';
   public role = 'GUEST';
   public payUrl = '';
+  public pendingPayment: {id?: number, amount?: number} = {};
 
-  constructor(private authService: AuthService, private router: Router) 
+  constructor(private authService: AuthService,
+    private router: Router,
+    private http: HttpClient) 
   { 
       this.email = this.authService.getEmail();
       this.role = this.authService.getRole();
       this.payUrl = environment.payServiceUrl;
+      this.http.get(environment.host + '/api/payservice/pending/' + this.email).subscribe(result => {
+        this.pendingPayment = (result as any).pending_payment;
+        console.log(this.pendingPayment);
+      });
   }
 
+  ngOnChanges(): void {
+      console.log('?=> run');
+  }
   cleanJWT(): void {
     this.authService.token = null;
+  }
+
+  deletePayment(id?: number): void {
+    this.http.get(environment.host + '/api/payservice/cancel/' + this.email + '/' + id).subscribe(result => {
+      if (this.pendingPayment.id === (result as any).pending_payment.id) {
+        this.pendingPayment = {};
+      }
+    });
   }
 }
