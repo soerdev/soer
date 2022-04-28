61cb5aa7f3ee01d923b2a6b611936c6d33f958f8
Evgeniy Sergeev <esergeev777@gmail.com>
2022-04-28 18:12:22 +0700

    fix: template creation for old targets

--- apps/naris/src/app/pages/modules/targets/targets.helpers.ts
+++ apps/naris/src/app/pages/modules/targets/targets.helpers.ts
@@ -7,7 +7,11 @@ export function target2template(target: TargetModel): TemplateModel {
     const template: TemplateModel = JSON.parse(JSON.stringify(target));
     const progressClear = (tmpTask: TemplateModel) => {
         tmpTask.progress = 0;
-        tmpTask.tasks.forEach(progressClear);
+        if (tmpTask.tasks) {
+            tmpTask.tasks?.forEach(progressClear);
+        } else {
+            tmpTask.tasks = [];
+        }
     }
     progressClear(template);
     return template;
