76dd70bb36b5f1821efa7a3ed6da5fa125b4c21f
Evgeniy Sergeev <esergeev777@gmail.com>
2022-06-10 14:04:17 +0700

    chore: fix jest problems

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,12 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset };
+module.exports = { 
+    ...nxPreset,
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+    '@soer/sr-auth': '<rootDir>/../../libs/sr-auth/src/index.ts',
+    '@soer/mixed-bus': '<rootDir>/../../libs/mixed-bus/src/index.ts',
+    '@soer/sr-dto': '<rootDir>/../../libs/sr-dto/src/index.ts',
+    '@soer/sr-url-builder': '<rootDir>/../../libs/sr-url-builder/src/index.ts',
+  }
+};
