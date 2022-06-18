4dd0cdaec906e12c3777dd71afb50988698113e0
Evgeniy Sergeev <esergeev777@yandex.ru>
2022-06-18 08:17:52 +0300

    chore: add WatchVideoEvent

--- apps/naris/src/app/api/progress/personal-activity.service.spec.ts
+++ apps/naris/src/app/api/progress/personal-activity.service.spec.ts
@@ -1,13 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-import { ANY_SERVICE, MixedBusService } from '@soer/mixed-bus';
-import { ChangeDataEvent, DataStoreService, OK, SrDTOModule } from '@soer/sr-dto';
+import { ANY_SERVICE, isBusMessage, MixedBusService } from '@soer/mixed-bus';
+import { ChangeDataEvent, CommandCreate, DataStoreService, OK, SrDTOModule } from '@soer/sr-dto';
+import { WatchVideoEvent } from './events/watch-video.event';
 
 import { PersonalActivity, PersonalActivityService } from './personal-activity.service';
 
 describe('PersonalActivityService', () => {
   let personalActivityService: PersonalActivityService;
-  const bus$ = new MixedBusService();
-  const store$ = new DataStoreService(bus$);
+  let bus$ = new MixedBusService();
+  let store$ = new DataStoreService(bus$);
 
   const fakePersonalActivity: PersonalActivity = {
     watched: {
@@ -16,6 +17,8 @@ describe('PersonalActivityService', () => {
   }
 
   beforeEach(() => {
+    bus$ = new MixedBusService();
+    store$ = new DataStoreService(bus$);
     TestBed.configureTestingModule({ imports: [SrDTOModule],
       providers: [
         {provide: MixedBusService, useValue: bus$},
@@ -47,4 +50,26 @@ describe('PersonalActivityService', () => {
   });
 
   
+  it('should recive WatchVideoEvent', () => {
+    const sub = bus$.of(CommandCreate).subscribe(data => {
+      if (isBusMessage(data)){
+        bus$.publish(
+          new ChangeDataEvent(
+              ANY_SERVICE, 
+              {
+                status: OK, 
+                items:[
+                  data.payload
+                ]
+              }
+          )
+        );
+      }
+    });
+    bus$.publish(new WatchVideoEvent(ANY_SERVICE, fakePersonalActivity.watched.videos[0]));
+    bus$.publish(new WatchVideoEvent(ANY_SERVICE, fakePersonalActivity.watched.videos[1]));
+    bus$.publish(new WatchVideoEvent(ANY_SERVICE, fakePersonalActivity.watched.videos[1]));
+    sub.unsubscribe();
+    expect(personalActivityService.getWatchedVideos()).toMatchObject(fakePersonalActivity.watched.videos);
+  });
 });
